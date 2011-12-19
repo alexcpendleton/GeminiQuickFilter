d78ca064f837ce4e6d6dc18d8b0f1de1d231572c
alexcpendleton@gmail.com <alexcpendleton@gmail.com>
2011-12-19 18:26:42 -0500

    It sort of works. The delay is for when you first load the dashboard. Need to find a way to hook into that ajax call or something so I can get the search box to reload after you switch tabs

--- chrome/main.js
+++ chrome/main.js
@@ -3,10 +3,53 @@
 // $(document).ready(function() {
 	// $("#TabData").on("ready", go);
 // });
-
-function go() {
-	var x = document.getElementById("TabData");
-	if(x) {
-		x.background = "red";
+$(function() {
+	//var tabdata = $("#TabData");
+	var idIncrementer = 0, 
+		searchTextBoxIDPrefix = "gqs_",
+		gridSelector = ".dataGrid, .dataTables_wrapper",
+		delay = 2000;
+	
+	var masterBody = $("#master-body");
+	function setupDataGrid() {
+		var t = $(this);
 	}
-}
+	
+	function makeSearchTextBoxID(incr) {
+		return searchTextBoxIDPrefix + incr;
+	}
+	
+	function createSearchContainer() {
+		var div = document.createElement("div");
+		div.appendChild(document.createTextNode("Search: "));
+		var textbox = document.createElement("input");
+		textbox.type = "text";
+		idIncrementer = idIncrementer + 1;
+		textbox.id = makeSearchTextBoxID(idIncrementer);
+		div.appendChild(textbox);
+		return div;
+	}
+	//$("#ctl00_Main_TabsTable").on("DOMNodeInserted", go);
+	//tabdata.get(0).addEventListener ('DOMNodeInserted', go, false);
+	
+	function applyGrid() {
+		var container = createSearchContainer()
+		$(this).before(container);
+		$(container).find("input").quicksearch($(this).find("tbody tr"));
+	}
+	
+	function applyToAllGrids() {
+		masterBody.find(gridSelector).each(function() {
+			applyGrid.call(this);
+		});
+	}
+	
+	setTimeout(function() {
+		applyToAllGrids();
+	}, delay);
+	
+	// Special cases for dashboard, doesn't work right yet
+	$(".tabItem").click(function() {
+		setTimeout(applyToAllGrids, delay);
+	});
+});
